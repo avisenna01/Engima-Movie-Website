9e2297f3b14c89d507c955df790fbb0354023b00
Ft-N <ftnfata@gmail.com>
2019-09-17 21:55:33 +0700

    add database loader

---
+++ public/load.php
@@ -0,0 +1,16 @@
+<?php
+$sql = file_get_contents('schema.sql');
+
+$mysqli = new mysqli("localhost", "root", "", "engima");
+if (mysqli_connect_errno()) { /* check connection */
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+/* execute multi query */
+if ($mysqli->multi_query($sql)) {
+    echo "success";
+} else {
+   echo "error";
+}
+?>
