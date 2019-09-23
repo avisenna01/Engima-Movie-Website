df4dffa8cd194c802b0bec32cd6e222df4e50ea7
Ft-N <ftnfata@gmail.com>
2019-09-23 11:03:03 +0700

    refoldering

--- public/index.php
+++ public/index.php
@@ -2,16 +2,19 @@
 	$request = $_SERVER['REQUEST_URI'];
 	switch ($request) {
 	    case '/' :
-	        require __DIR__ . '/../views/index.html';
+	        require __DIR__ . '/../app/views/index.html';
 	        break;
 	    case '' :
-	        require __DIR__ . '/../views/index.html';
+	        require __DIR__ . '/../app/views/index.html';
 	        break;
 	    case '/connect' :
-	        require __DIR__ . '/../controller/connectTest.php';
+	        require __DIR__ . '/../app/controller/connectTest.php';
 	        break;
 	    case '/load' :
-	        require __DIR__ . '/../controller/load.php';
+	        require __DIR__ . '/../app/controller/load.php';
+	        break;
+	    case '/head' :
+	        require __DIR__ . '/../app/views/header.html';
 	        break;
 	    default:
 	        require __DIR__ . '/../views/fail.html';
