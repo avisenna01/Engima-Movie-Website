524d2443fa043e7362785d246a1972387c655bca
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2019-09-24 07:30:56 +0700

    add modal after checkout

--- views/js/booking.js
+++ views/js/booking.js
@@ -1,7 +1,10 @@
 const buttons = document.getElementsByTagName("button");
 
 let seatDetails = document.createElement("div");
+let buyTicketSection = document.createElement("div");
+let seatNumberAndPrice = document.createElement("div");
 let summary = document.getElementsByClassName("summary");
+let modal = document.getElementsByClassName("modal");
 
 let movieTitle = createNewElement("h3", "Avengers: Endgame");
 let movieDate = createNewElement("p", "Septemver 4, 2019 - 09:40");
@@ -9,20 +12,30 @@ let seatNumber = createNewElement("h3", "Seat #X");
 let price = createNewElement("h3", "Rp 75.000,-");
 let buyTicketButton = createNewElement("button", "Buy Ticket");
 
+appendArrayOfChild(seatNumberAndPrice, [seatNumber, price]);
+seatNumberAndPrice.setAttribute("class", "number-price");
+
+buyTicketButton.setAttribute("class", "blue-button");
+buyTicketSection.setAttribute("class", "buy-ticket-section");
+appendArrayOfChild(buyTicketSection, [buyTicketButton]);
+
 appendArrayOfChild(seatDetails, [
   movieTitle,
   movieDate,
-  seatNumber,
-  price,
-  buyTicketButton
+  seatNumberAndPrice,
+  buyTicketSection
 ]);
 
+buyTicketButton.addEventListener("click", successMessage);
+
 for (i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", checkout);
+  buttons[i].addEventListener("click", checkout(i));
 }
 
-function checkout() {
+function checkout(i) {
   summary[0].replaceChild(seatDetails, summary[0].lastElementChild);
+  // let numberAndPrice = document.getElementsByClassName("number-price");
+  // numberAndPrice[0].firstElementChild.textContent = `Seat #${i}`;
 }
 
 function createNewElement(element, text) {
@@ -35,4 +48,8 @@ function appendArrayOfChild(element, arrayChild) {
   arrayChild.map(child => {
     element.appendChild(child);
   });
+}
+
+function successMessage() {
+  modal[0].style.display = "block";
 }
