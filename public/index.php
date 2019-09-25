a7cf6e694f81c3e5ce38d37775cc556747615626
Ft-N <ftnfata@gmail.com>
2019-09-25 17:15:31 +0700

    header ready to merge

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@
 	        require __DIR__ . '/../app/views/header.html';
 	        break;
 	    default:
-	        require __DIR__ . '/../views/fail.html';
+	        require __DIR__ . '/../app/views/fail.html';
 	        break;
 	}
 
