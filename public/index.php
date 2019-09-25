a4db0a19ec8b48089901b9e52a9491b12720faf1
Ft-N <ftnfata@gmail.com>
2019-09-25 17:06:11 +0700

    prepare merge header

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 	$request = $_SERVER['REQUEST_URI'];
 	switch ($request) {
 	    case '/' :
-	        require __DIR__ . '/../app/views/homepage.html';
+	        require __DIR__ . '/../app/views/index.html';
 	        break;
 	    case '' :
-	        require __DIR__ . '/../app/views/homepage.html';
+	        require __DIR__ . '/../app/views/index.html';
 	        break;
 	    case '/connect' :
 	        require __DIR__ . '/../app/controller/connectTest.php';
