6784c932842a63774e1b8c237eec84f61c73a014
Ft-N <ftnfata@gmail.com>
2019-09-28 19:25:44 +0700

    basic cookie in js

--- public/js/get.js
+++ public/js/get.js
@@ -1,12 +1,21 @@
 function getJSON() {
+    var userid = getCookie("userid");
+    if (userid != "") {
+        location.replace("http://localhost:8080");
+    } else {
+        userid = prompt("Please enter your id:", "");
+        if (userid != "" && userid != null) {
+            setCookie("username", userid);
+        }
+    }
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        [myObj] = myObj;
-        document.getElementById("username").innerHTML = myObj.username;
-      }
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            [myObj] = myObj;
+            document.getElementById("username").innerHTML = myObj.username;
+        }
     };
-    xmlhttp.open("GET", "/api/user/getUserName?id=1", true);
+    xmlhttp.open("GET", "/api/user/getUserName?id=" + userid, true);
     xmlhttp.send();
 };
