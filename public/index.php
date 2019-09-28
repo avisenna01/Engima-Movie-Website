a5e3c5281471e48e52830e3476969e2219068e7f
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-09-29 00:18:26 +0700

    update homepage

--- public/index.php
+++ public/index.php
@@ -2,20 +2,17 @@
 	$request = $_SERVER['REQUEST_URI'];
 	switch ($request) {
 	    case '/' :
-	        require __DIR__ . '/../app/views/index.html';
+	        require __DIR__ . '/../app/views/homepage.html';
 	        break;
 	    case '' :
-	        require __DIR__ . '/../app/views/index.html';
-	        break;
-	    case '/connect' :
-	        require __DIR__ . '/../app/controller/connectTest.php';
+	        require __DIR__ . '/../app/views/homepage.html';
 	        break;
 	    case '/load' :
 	        require __DIR__ . '/../app/controller/load.php';
 	        break;
 	    case '/head' :
 	        require __DIR__ . '/../app/views/header.html';
-	        break;
+			break;
 	    default:
 	        require __DIR__ . '/../app/views/fail.html';
 	        break;
