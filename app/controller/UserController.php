22354e92f7f76d8e7191b474f47519649c60765f
Ft-N <ftnfata@gmail.com>
2019-09-29 09:31:18 +0700

    divide task

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -6,4 +6,46 @@ class UserController extends Controller{
 		$this->db->bind('id', $_GET['id']);
 		echo json_encode($this->db->result());
 	}
+
+	public function login()
+	{
+		$query = "SELECT * FROM user WHERE email=:email and password=:password";
+		$this->db->query($query);
+		$this->db->bind('email', $_POST['uname']);
+		$this->db->bind('password', $_POST['psw']);
+		echo json_encode($this->db->result());
+	}
+
+	public function register()
+	{
+		$query = "insert into ....";
+		$this->db->query($query);
+		$this->db->bind('email', $_POST['uname']);
+		$this->db->bind('password', $_POST['psw']);
+	}
+
+	public function checkuname()
+	{
+		$query = "SELECT * FROM user WHERE username=:username";
+		$this->db->query($query);
+		$this->db->bind('username', $_GET['uname']);
+		echo json_encode($this->db->result());
+	}
+
+//email
+	public function checkuname()
+	{
+		$query = "SELECT * FROM user WHERE username=:username";
+		$this->db->query($query);
+		$this->db->bind('username', $_POST['uname']);
+		echo json_encode($this->db->result());
+	}
+//phone
+	public function checkuname()
+	{
+		$query = "SELECT * FROM user WHERE username=:username";
+		$this->db->query($query);
+		$this->db->bind('username', $_POST['uname']);
+		echo json_encode($this->db->result());
+	}
 }
