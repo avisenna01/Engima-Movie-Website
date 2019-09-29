f16edfe073983bf6d4a5045a0b08baa06052ba19
Nathaniel Evan <13516055@std.stei.itb.ac.id>
2019-09-29 11:11:56 +0700

    add get request in booking

--- public/js/booking.js
+++ public/js/booking.js
@@ -5,42 +5,53 @@ let summary = document.getElementsByClassName("summary");
 let seatSection = document.querySelectorAll(".seat-section li button");
 let transButton = document.getElementById('trans-button');
 
-transButton.addEventListener("click", redirect);
+function restructureSeatButton(seats) {
+  let ol = document.getElementsByClassName("seat-section");
 
-function redirect() {
-  location.replace("http://localhost:8080/transaction");
+  seats.map(seat => {
+    let button = document.createElement("button");
+    let li = document.createElement("li");
+
+    button.innerHTML = seat.chair_number;
+    li.appendChild(button);
+    ol.appendChild(li);
+  });
 }
 
-console.log(seatSection);
-
-
-
-buyTicketButton[0].addEventListener("click", successMessage);
-
-for (i = 0; i < seatSection.length; i++) {
-  seatSection[i].addEventListener("click", checkout);
+function getAllSeats() {
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let myObj = JSON.parse(this.responseText);
+      [myObj] = myObj;
+      if (myObj != null) {
+        return myObj;
+      }
+    }
+  };
+  xmlhttp.open("GET", "/api/seat/getAllSeats", true);
+  xmlhttp.send();
 }
 
 function checkout() {
-  console.log(summary[0].children);
   summary[0].children[1].style.display = "none";
   seatDetails[0].style.display = "block";
   let numberAndPrice = document.getElementsByClassName("number-price");
   numberAndPrice[0].firstElementChild.textContent = "Seat #" + this.id;
 }
 
-function createNewElement(element, text) {
-  let newElement = document.createElement(element);
-  newElement.textContent = text;
-  return newElement;
-}
-
-function appendArrayOfChild(element, arrayChild) {
-  arrayChild.map(child => {
-    element.appendChild(child);
-  });
-}
-
 function successMessage() {
   modal[0].style.display = "block";
+}
+
+function redirect() {
+  location.replace("http://localhost:8080/transaction");
+}
+
+transButton.addEventListener("click", redirect);
+
+buyTicketButton[0].addEventListener("click", successMessage);
+
+for (i = 0; i < seatSection.length; i++) {
+  seatSection[i].addEventListener("click", checkout);
 }
