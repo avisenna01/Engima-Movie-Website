4dbd0f57ae59c47c5a0aa9e336a43dec9f783942
Nathaniel Evan <13516055@std.stei.itb.ac.id>
2019-09-29 13:31:02 +0700

    resolve conflict

--- public/index.php
+++ public/index.php
@@ -13,10 +13,40 @@
 	        break;
 	    case 'head' :
 	        require __DIR__ . '/../app/views/header.html';
-			break;
-		case '/movie' :
-			require __DIR__ . '/../app/views/movie-desc.html';
-			break;
+	        break;
+	    case 'movie' :
+	        require __DIR__ . '/../app/views/movie-desc.html';
+	        break;
+	    case 'test' :
+	        require __DIR__ . '/../app/views/index.html';
+	        break;
+	    case 'api':
+	    	switch ($url[1]) {
+	    		case 'user':
+	    		case 'film':
+	    		case 'schedule':
+	    		case 'transaction':
+	    		case 'seat':
+	    			$controller = $url[1].'controller';
+	    			require_once '../app/controller/'.$controller.'.php';
+	    			$controller = new $controller();
+	    			$method = 'fail';
+	    			if( isset($url[2]) ) {
+			            if( method_exists($controller, $url[2]) ) {
+			                $method = $url[2];
+			            }
+			        }
+	    			call_user_func_array([$controller, $method], []);
+	    			break;
+	    		default:
+	    			$response = [
+	    				"status" => "404",
+	    				"message" => "Error API not found"
+	    			];
+	    			echo json_encode($response);
+	    			break;
+	    	}
+	    	break;
 	    default:
 	        require __DIR__ . '/../app/views/fail.html';
 	        break;
